f84d7576cc9b331455b1b7e85d288d1775e23b5c
Arthur Song <arth3rs0ng@gmail.com>
2019-06-02 03:46:59 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -64,6 +64,7 @@ class TweetsController < ApplicationController
   post '/tweets/:id/delete' do
     user = current_user
     tweet = Tweet.find(params[:id])
+    binding.pry
     if user.tweets.include?(tweet)
       user.tweets.destroy(Tweet.find(params[:id]))
       user.save
